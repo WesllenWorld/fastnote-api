b06e0f89e24b001cc8388b8b7aad41591facd2b3
WesllenWorld <wesllen.2020111052@unicap.br>
2024-09-23 12:04:58 -0300

    created tags and users controllers with signatures

---
+++ src/controllers/tags-controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express"
+import * as tagsService from '../services/tags-service'
+
+export const getTags = async (request:Request, response:Response) => {
+    
+}
+
+export const getTagByUserId = async (request:Request, response:Response) => {
+    
+}
+
+export const postTag = async (request:Request, response:Response) => {
+    
+}
+
+export const deleteTag = async (request:Request, response:Response) => {
+    
+}
+
+export const updateTag = async (request:Request, response:Response) => {
+    
+}
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express"
+import * as usersService from '../services/users-service'
+
+export const getUsers = async (request:Request, response:Response) => {
+    
+}
+
+export const getUserByUserId = async (request:Request, response:Response) => {
+    
+}
+
+export const postUser = async (request:Request, response:Response) => {
+    
+}
+
+export const deleteUser = async (request:Request, response:Response) => {
+    
+}
+
+export const updateUser = async (request:Request, response:Response) => {
+    
+}
