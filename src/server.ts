3441e7b10cb4081e975f90708e2572cdc1ea112c
WesllenWorld <wesllen.2020111052@unicap.br>
2024-09-30 12:20:26 -0300

    server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import createApp from './application/app'
-import { AppDataSource } from './data-source'
+import { AppDataSource } from './db/data-source'
+//import "reflect-metadata"
 
 const app = createApp()
 const port = process.env.PORT
 
 
-AppDataSource.initialize().then(async () =>{
+AppDataSource.initialize().then(async () => {
     app.listen(port, () => {
-    console.log(`Server running at http:localhost:${port}`)	    
+        console.log(`Server running at http://localhost:${port}`)
     })
-    console.log('Database connected')
 }).catch((error) => {
-    console.log('Error connecting to database', error)
+    console.log(error)
 })
